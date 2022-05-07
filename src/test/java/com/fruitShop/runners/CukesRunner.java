d3ae858264cd6f3ecb38eb4b34e83eec701e0ba6
aykhanakhundov <aykhanakhundov2021@gmail.com>
2022-05-07 19:15:30 -0400

    CukesRunner added

---
+++ src/test/java/com/fruitShop/runners/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.fruitShop.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "com/fruitShop/stepDefinitions",
+        dryRun = false,
+        tags = ""
+)
+public class CukesRunner {
+}
